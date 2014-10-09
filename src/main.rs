de9a81e16fee7a981069b6ad7cb247b8022f46e4
Huon Wilson <dbau.pp+github@gmail.com>
2014-10-09 22:55:35 +1100

    Add logging.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,27 @@
 extern crate nickel;
 extern crate slow_primes;
+extern crate time;
 
+use std::io::{mod, File};
 use std::io::net::ip::Ipv4Addr;
-use nickel::{ Nickel, Request, Response, HttpRouter };
+use nickel::{ Nickel, Request, Response, HttpRouter, MiddlewareResult };
+
+static LOG_FILE: &'static str = "requests.log";
 
 fn main() {
     let mut server = Nickel::new();
+
+    fn log(request: &Request, _response: &mut Response) -> MiddlewareResult {
+        (writeln!(File::open_mode(&Path::new(LOG_FILE), io::Append, io::Write).unwrap(),
+                  "{time}\t{ip}\t{uri}",
+                  time = time::now_utc().rfc3339(),
+                  ip = request.origin.remote_addr,
+                  uri = request.origin.request_uri))
+            .unwrap();
+
+
+        Ok(nickel::Continue)
+    }
 
     fn root(_request: &Request, response: &mut Response) {
         response.content_type("html");
@@ -34,10 +50,12 @@ fn main() {
         }
     }
 
+    server.utilize(log);
+
     server.get("/", root);
     server.get("/style.css", style);
     server.get("/script.js", script);
     server.get("/is_prime/:number", is_prime);
 
-    server.listen(Ipv4Addr(0, 0, 0, 0), 80);
+    server.listen(Ipv4Addr(0, 0, 0, 0), 5000);
 }
